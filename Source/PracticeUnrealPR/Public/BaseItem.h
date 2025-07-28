13151fa75910717f3ed7b53448c4044d03342952
oSiris1o7 <siuesen@gmail.com>
2025-07-28 20:06:47 +0900

    #5 [태그는뭐라고해야할지] ItemID 추가

--- Source/PracticeUnrealPR/Public/BaseItem.h
+++ Source/PracticeUnrealPR/Public/BaseItem.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
@@ -17,4 +17,8 @@ protected:
 
 public:	
 	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	int ItemID;
 };
